075832320e8e8523f17f12078c6b976c87c6d870
stf45 <stf45@drexel.edu>
2022-05-16 13:30:13 -0400

    adding new file

--- python/social_serv_sic_check.py
+++ python/social_serv_sic_check.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 """
+Created on Mon May 16 11:25:31 2022
+
+@author: stf45
+"""
+
+# -*- coding: utf-8 -*-
+"""
 Created on Mon May 16 11:05:02 2022
 
 @author: stf45
@@ -17,6 +24,7 @@ def merge_sic_emp_sales_misc(sic_chunk, emp_chunk, sales_chunk, company_chunk, m
     misc_merge = emp_merge.merge(misc_chunk, on='DunsNumber')
     classification_wide = pd.merge(misc_merge, sales_chunk, on='DunsNumber', how='left')
     return classification_wide
+
 #%%
 # FULL FILES:           
 n = 71498225
@@ -24,8 +32,7 @@ chunksize = 10000000
 
 sic_reader = pd.read_csv(r'D:\NETS\NETS_2019\RawData\NETS2019_SIC.txt', sep = '\t', dtype={"DunsNumber": str, 'SIC19':str},  header=0, chunksize=chunksize, encoding_errors='replace', usecols=["DunsNumber",
                                                                                                                                                                               "SIC19"])
-
-                                                                                                                                                          
+                                                                                                                                                   
 company_reader = pd.read_csv(r'D:\NETS\NETS_2019\RawData\NETS2019_Company.txt', sep = '\t', dtype={"DunsNumber": str},  header=0, chunksize=chunksize, encoding_errors='replace', usecols=["DunsNumber",
                                                                                                                                                                     "Company",
                                                                                                                                                                     "TradeName",
@@ -34,8 +41,6 @@ company_reader = pd.read_csv(r'D:\NETS\NETS_2019\RawData\NETS2019_Company.txt', 
                                                                                                                                                                     "State",
                                                                                                                                                                     "ZipCode"])
 emp_reader = pd.read_csv(r'D:\NETS\NETS_2019\RawData\NETS2019_Emp.txt', sep = '\t', dtype={"DunsNumber": str},  header=0, chunksize=chunksize, encoding_errors='replace', usecols=["DunsNumber", "Emp19"])
-
-                                                                                                                                                                                  
 
 sales_reader = pd.read_csv(r'D:\NETS\NETS_2019\RawData\NETS2019_Sales.txt', sep = '\t', dtype={"DunsNumber": str},  header=0, chunksize=chunksize, encoding_errors='replace', usecols=["DunsNumber", "Sales19"])
                       
@@ -49,9 +54,9 @@ time_list = []
 for c, (sic_chunk, emp_chunk, sales_chunk, company_chunk, misc_chunk) in enumerate(readers):
     tic = time.perf_counter()
     header = (c==0)
-    sic_chunk = sic_chunk[sic_chunk['SIC19'] == '60999901']
+    sic_chunk = sic_chunk[sic_chunk['SIC19'].str.startswith('8399', na=False)]
     sic_check_wide = merge_sic_emp_sales_misc(sic_chunk, emp_chunk, sales_chunk, company_chunk, misc_chunk)
-    sic_check_wide.to_csv(r"C:\\Users\\stf45\\Documents\\NETS\\Processing/scratch/check_cashing_check.txt", sep="\t", header=header, mode='a', index=False)
+    sic_check_wide.to_csv(r"C:\\Users\\stf45\\Documents\\NETS\\Processing/scratch/social_serv_check.txt", sep="\t", header=header, mode='a', index=False)
     toc = time.perf_counter()
     t = toc - tic
     time_list.append(t)
@@ -63,10 +68,13 @@ print(runtime)
 
 #%% READ IN CSV
 
-check_cash = pd.read_csv(r"C:\\Users\\stf45\\Documents\\NETS\\Processing/scratch/check_cashing_check.txt", sep = '\t', dtype={"DunsNumber": str},  header=0)
+social_serv = pd.read_csv(r"C:\\Users\\stf45\\Documents\\NETS\\Processing/scratch/social_serv_check.txt", sep = '\t', dtype={"DunsNumber": str},  header=0)
 
 
 #%% WRITE TABLE TO EXCEL
 
-with pd.ExcelWriter(r'C:\Users\stf45\Documents\NETS\Processing\scratch\check_cash_check_20220516.xlsx') as writer:
-    check_cash.to_excel(writer, "records w check cashing sic", index=False)
+with pd.ExcelWriter(r'C:\Users\stf45\Documents\NETS\Processing\scratch\social_serv_check_20220516.xlsx') as writer:
+    social_serv.to_excel(writer, "records w sic 8399 (2019)", index=False)
+
+
+
