dfd928c93d2ece6453377067bc3274ac0e58cf65
stf45 <stf45@drexel.edu>
2023-12-07 15:25:19 -0800

    updated for nets 2022. doesnt need check on contigous us tracts/landarea because file read in is all contiguous us tracts and contains no tracts with ALAND10 == 0

--- python/measures/backfill_tract_measures.py
+++ python/measures/backfill_tract_measures.py
@@ -6,33 +6,20 @@ Created on Wed Aug 23 09:10:41 2023
 """
 
 import pandas as pd
-from sas7bdat import SAS7BDAT
 
 #%% READ IN TRACT LEVEL MEASURES FILE
 
-df = pd.read_csv(r'\\files.drexel.edu\colleges\SOPH\Shared\UHC\Projects\NETS\Data\NETS2019_Python\NETS_tr10_measures20230823.txt', sep='\t', dtype={'tract10':str})
+df = pd.read_csv(r'D:\scratch\NETS_tr10_measures20231207.txt', sep='\t', dtype={'tract10':str})
 
-with SAS7BDAT(r"Z:\UHC_Data\Census2010\data\sf1_census2010_tract.sas7bdat") as file:
-    alltracts10 = file.to_data_frame() 
-
-with SAS7BDAT(r"Z:\UHC_Data\CensusACS15_19\data\tractchange_10_19.sas7bdat") as file:
-    tractchange = file.to_data_frame() 
-
-stevetracts = pd.read_csv(r'C:\Users\stf45\Downloads\missingtr10measures20230913.csv', dtype={'GEOID10':str})
-
-#%% REMOVE NON-CONTIG AND ZERO LAND AREA TRACTS
-
-alltracts10 = alltracts10[['GEOID10', 'AREALAND']]
-noncontig = alltracts10.loc[~alltracts10['GEOID10'].str[:2].isin(['02', '15', '72'])]
-noncontig = noncontig.loc[noncontig['AREALAND'] > 0]
-noncontig = noncontig.drop(columns=['AREALAND'])
+# load file with all census tracts in the contiguous us.
+alltracts10 = pd.read_csv(r'Z:\UHC_Data\Census2010\Geodatabases\tblCT_2010_ContUSLandRepairAlbers.csv', usecols=['GEOID10'], dtype={'GEOID10':str})
 
 #%% CREATE DF OF ALL TRACTS IN ALL YEARS
 
 tractyear = pd.DataFrame()
-years = range(1990,2020)
+years = range(1990,2023)
 for year in years:
-    newdf = noncontig.copy()
+    newdf = alltracts10.copy()
     newdf['Year'] = year
     tractyear = pd.concat([tractyear,newdf])
 
@@ -45,12 +32,7 @@ df2 = df2.fillna(0)
 
 df2 = df2.drop(columns='tract10')
 df2 = df2.rename(columns={'GEOID10':'tract10'})
-df2.head()
+df2head = df2.head()
 
 # export to csv
-df2.to_csv(r'\\files.drexel.edu\colleges\SOPH\Shared\UHC\Projects\NETS\Data\NETS2019_Python\NETS_tr10_measures20230914.txt', sep='\t', index=False)
-
-#%%
-
-notin19.to_csv(r'C:\Users\stf45\Documents\NETS\Processing\scratch\tracts2010_notin2019.csv', index=False)
-notin10.to_csv(r'C:\Users\stf45\Documents\NETS\Processing\scratch\tracts2019_notin2010.csv', index=False)
+df2.to_csv(r'D:\scratch\NETS_tr10_measuresYYYYMMDD.txt', sep='\t', index=False)
